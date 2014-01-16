62fde1c330752c6c9a7b086926714eb7965b48c3
n0ts <me@n0ts.org>
2014-01-16 11:34:59 +0900

    Fixed a bit

--- 00-init.sh
+++ 00-init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-URL="https://raw2.github.com/n0ts/os-x/master/"
-SCRIPTS=(01-hostname 10-xcode 20-boxen)
+URL="https://raw.github.com/n0ts/os-x/master/"
 
-for script in $SCRIPTS; do
+SCRIPTS=(01-hostname 10-xcode)
+for script in ${SCRIPTS[@]}; do
     curl -Ls $URL/$script.sh | bash -
 done
