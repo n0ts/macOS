05afb8453a218e05e89fdd0bf3cc23296843151d
n0ts <me@n0ts.org>
2014-01-16 11:24:30 +0900

    Fixed a curl option

--- 00-init.sh
+++ 00-init.sh
@@ -4,5 +4,5 @@ URL="https://raw2.github.com/n0ts/os-x/master/"
 SCRIPTS=(01-hostname 10-xcode 20-boxen)
 
 for script in $SCRIPTS; do
-  curl $URL/$script.sh | bash -
+    curl -Ls $URL/$script.sh | bash -
 done
