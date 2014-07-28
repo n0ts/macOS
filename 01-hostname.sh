d774def8f19537a2895ee12b90db4030e09def4a
n0ts <me@n0ts.org>
2014-07-28 16:53:40 +0900

    Fix a typo

--- 01-hostname.sh
+++ 01-hostname.sh
@@ -8,6 +8,6 @@ fi
 SCUTIL=/usr/sbin/scutil
 sudo $SCUTIL --set ComputerName $HOSTNAME
 sudo $SCUTIL --set HostName $HOSTNAME.local
-sudo $SCUTIL --get LocalHostName $HOSTNAME
+sudo $SCUTIL --set LocalHostName $HOSTNAME
 
 exit 0
